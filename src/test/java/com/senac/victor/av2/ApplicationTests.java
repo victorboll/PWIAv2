dffb5a82e0228fd5017aec05fc7d5a3f5d1b1559
Victor Bollentini <victorboll@yahoo.com.br>
2024-11-26 16:58:15 -0300

    Primeiro Commit

---
+++ src/test/java/com/senac/victor/av2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.victor.av2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
